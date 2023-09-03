638adeed1175fe9ef0ae7d9c4a4d2fea796ef3f9
SZSage <simonz@uoregon.edu>
2023-09-02 17:06:48 -0700

    all tests passed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...value) {
+	// set array to a variable
+	// loop through array
+	// if value is in array remove from array and return
+	let arr = array;
+	let newArr = [];
+	for (let i = 0; i < arr.length; i++) {
+		if (!value.includes(arr[i])) {
+			newArr.push(arr[i]);
+		}
+	}
+	return newArr;
 };
 
 // Do not edit below this line
