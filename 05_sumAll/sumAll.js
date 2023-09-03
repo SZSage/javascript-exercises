b58b49ba4d9aafaa64e9d67422143d5698a50304
SZSage <simonz@uoregon.edu>
2023-09-02 20:16:57 -0700

    Test passed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,35 @@
-const sumAll = function() {
-
+const sumAll = function(from, to) {
+	let sum = 0;
+	if (from > to) {
+		for (let i = to; i <= from; i++) {
+			sum += i;
+		}
+		return sum;
+	} else if ((from < 0 || to < 0)) {
+		return "ERROR";
+	} else if (isStr(from, to) || isNotNum(from, to)) {
+		return "ERROR";
+	} else {
+		for (let i = from; i <= to; i++) {
+			sum += i;
+		}
+		return sum;
+	}
 };
+
+function isStr(from, to) {
+	if (typeof from === "string" || typeof to === "string") {
+		return "ERROR";
+	}
+}
+
+function isNotNum(from, to) {
+	if (isNaN(from) || isNaN(to)) {
+		return "ERROR";
+	}
+}
 
 // Do not edit below this line
 module.exports = sumAll;
+
+
