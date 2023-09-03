8d280e0a011b94a66f98a37576ad31c77a80edb0
SZSage <simonz@uoregon.edu>
2023-09-03 14:07:41 -0700

    all test passedd

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,33 +1,53 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let arr = array;
+	let sum = 0;
+	for (let i = 0; i < array.length; i++) {
+		sum += arr[i];
+	}
+	return sum;
 };
 
-const multiply = function() {
+const multiply = function(array) {
+	let arr = array;
+	let sum = 1;
+	for (let i = 0; i < array.length; i++) {
+		sum *= arr[i];
+	}
+	return sum;
+};
+
+const power = function(num1, power) {
+	return num1 ** power;
 
 };
 
-const power = function() {
-	
+const factorial = function(number) {
+	// given a number, store in variable
+	// create sum variable at 1
+	// start the loop at the number and decrement per loop
+	// i = 6
+	// multiply and store in sum
+	// decrement counter
+	let sum = 1;
+	for (let i = number; i > 0; i--) {
+		sum *= i;
+	}
+	return sum;
 };
-
-const factorial = function() {
-	
-};
-
 // Do not edit below this line
 module.exports = {
-  add,
-  subtract,
-  sum,
-  multiply,
-  power,
-  factorial
+	add,
+	subtract,
+	sum,
+	multiply,
+	power,
+	factorial
 };
