61b9c0001ac8a0c4caa53c9929c754fbf1e16d94
SZSage <simonz@uoregon.edu>
2023-09-04 16:07:31 -0700

    all tests passed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,27 @@
-const palindromes = function () {
+const palindromes = function(string) {
+	let str = string;
+	let cleanString = "";
+	let letters = /^[A-Za-z0-9]+$/;
+	for (let i = 0; i < string.length; i++) {
+		if (str[i].match(letters)) {
+			cleanString += str[i];
+		}
+		cleanString = cleanString.toLowerCase();
+	}
+	let start = 0;
+	let end = cleanString.length - 1;
+	while (start < end) {
+		if (cleanString[start] !== cleanString[end]) {
+			return false;
+		} else {
+			start++;
+			end--;
+		}
+	}
+	return true;
+}
 
-};
+
 
 // Do not edit below this line
 module.exports = palindromes;
